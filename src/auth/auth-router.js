be29a7bdc3a80a3b70159efff0a23ff679f62b4e
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2020-05-10 12:45:21 -0500

    implemented login

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -7,9 +7,11 @@ const jsonBodyParser = express.json()
 
 authRouter
   .post('/login', jsonBodyParser, (req, res, next) => {
-    const { user_name, password } = req.body
-    const loginUser = { user_name, password }
-
+    const { username, password } = req.body
+    // console.log(username)
+    
+    const loginUser = { username, password }
+    // console.log(loginUser)
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
         return res.status(400).json({
@@ -18,33 +20,41 @@ authRouter
 
     AuthService.getUserWithUserName(
       req.app.get('db'),
-      loginUser.user_name
+      loginUser.username
     )
       .then(dbUser => {
-        if (!dbUser)
+        console.log(dbUser)
+        // if (!dbUser)
+        //   return res.status(400).json({
+        //     error: 'Incorrect username or password',
+        //   })
+
+        // return AuthService.comparePasswords(loginUser.password, dbUser.password)
+        //   .then(compareMatch => {
+        //     if (!compareMatch)
+        //       return res.status(400).json({
+        //         error: 'Incorrect username or password',
+        //       })
+        if(loginUser.password === dbUser.password) {
+          const sub = dbUser.username
+          const payload = { user_id: dbUser.id }
+          console.log(payload)
+          res.send({
+            authToken: AuthService.createJwt(sub, payload),
+          })
+        } else {
           return res.status(400).json({
-            error: 'Incorrect user_name or password',
-          })
+                    error: 'Incorrect username or password',
+                  })
+        }
 
-        return AuthService.comparePasswords(loginUser.password, dbUser.password)
-          .then(compareMatch => {
-            if (!compareMatch)
-              return res.status(400).json({
-                error: 'Incorrect user_name or password',
-              })
-
-            const sub = dbUser.user_name
-            const payload = { user_id: dbUser.id }
-            res.send({
-              authToken: AuthService.createJwt(sub, payload),
-            })
-          })
+          // })
       })
       .catch(next)
   })
 
 authRouter.post('/refresh', requireAuth, (req, res) => {
-  const sub = req.user.user_name
+  const sub = req.user.username
   const payload = { user_id: req.user.id }
   res.send({
     authToken: AuthService.createJwt(sub, payload),
